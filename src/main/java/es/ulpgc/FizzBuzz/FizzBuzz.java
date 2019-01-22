354d3d805313f793adc21f16a6dcda0e5e39112c
C Gustavo Pérez Almécija <cargus96@hotmail.com>
2019-01-22 21:32:15 +0000

    Cambios

---
+++ src/main/java/es/ulpgc/FizzBuzz/FizzBuzz.java
@@ -0,0 +1,28 @@
+package es.ulpgc.FizzBuzz;
+
+public class FizzBuzz {
+    public String of(int number) {
+        String result = fizz(number) + buzz(number);
+        return result.isEmpty() ? getString(number) : result;
+    }
+
+    private String fizz(int number) {
+        return isMultipleOfThree(number) ? "Fizz" : "";
+    }
+    private String buzz(int number) {
+        return isMultipleOfFive(number) ? "Buzz" : "";
+    }
+
+    private boolean isMultipleOfFive(int number) {
+        boolean res =number % 5 == 0;
+        return res;
+    }
+
+    private boolean isMultipleOfThree(int number) {
+        return number % 3 == 0;
+    }
+
+    private String getString(int number) {
+        return String.valueOf(number);
+    }
+}
